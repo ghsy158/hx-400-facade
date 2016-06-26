344600edffa8661a36efd8f348f51a397ec86f5c
ghsy158 <fgh326@126.com>
2016-06-26 18:00:27 +0800

    添加同步当天工单

--- src/main/java/com/huaxia/call400/facade/Call400Facade.java
+++ src/main/java/com/huaxia/call400/facade/Call400Facade.java
@@ -1,5 +1,8 @@
 package com.huaxia.call400.facade;
 
+import java.sql.Date;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -7,6 +10,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
+import com.alibaba.fastjson.JSONObject;
 
 @Path("/call400Service")
 @Consumes({ MediaType.APPLICATION_JSON, MediaType.TEXT_XML })
@@ -18,7 +22,7 @@ public interface Call400Facade {
 	 * <b>概要说明：</b><br>
 	 */
 	@GET
-	@Path("/get400Calls")
+	@Path("/query400CallsAll")
 	public String get400Calls() throws Exception;
 
 	/**
@@ -26,5 +30,28 @@ public interface Call400Facade {
 	 * <b>方法名称：</b>插入到ERP商机表<br>
 	 * <b>概要说明：</b><br>
 	 */
-	public void batchInsert2Opp(String json)  throws Exception;
+	public void batchInsert2Opp(String json) throws Exception;
+
+	/**
+	 * 
+	 * <b>方法名称：</b>查询400工单当天的数据，并返回list<br>
+	 * <b>概要说明：</b><br>
+	 */
+	@GET
+	@Path("/query400CallsToday")
+	public List<JSONObject> query400CallsToday() throws Exception;
+
+	/**
+	 * 
+	 * <b>方法名称：</b>单笔插入工单信息到ERP商机表<br>
+	 * <b>概要说明：</b><br>
+	 */
+	public int singleInsert2Opp(JSONObject jsonObject) throws Exception;
+	
+	/**
+	 * 
+	 * <b>方法名称：</b>获取当前时间<br>
+	 * <b>概要说明：</b><br>
+	 */
+	public Date getCurrentTime();
 }
